6c4c55006521cc8ef8d8c136d2672f1a81056b3d
onik-999 <onikdas.dev@gmail.com>
2026-05-13 11:54:42 +0600

    add Delete Destination

--- src/app/destinations/[id]/page.jsx
+++ src/app/destinations/[id]/page.jsx
@@ -1,4 +1,5 @@
 import BookingCard from "@/components/BookingCard";
+import { DeleteAlert } from "@/components/DeleteAlert";
 import { EditModal } from "@/components/EditModal";
 import { Button } from "@heroui/react";
 import Image from "next/image";
@@ -19,7 +20,7 @@ const DestinationDetailsPage = async ({ params }) => {
     <div className="max-w-7xl mx-auto">
       <div className="flex  items-center gap-3 justify-end mt-5 mb-3">
         <EditModal destination={destination} />
-        {/* <DeleteAlert destination={destination}/> */}
+        <DeleteAlert destination={destination}/>
       </div>
       <Image
         className="w-full h-100 object-cover"
