b625555372eefcf7c700dd3813da4afe4a44ad32
onik-999 <onikdas.dev@gmail.com>
2026-05-15 14:03:20 +0600

    Creating My Booking page and fetching booking

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -1,9 +1,22 @@
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
 import React from 'react';
 
-const MyBookingPage = () => {
+const MyBookingPage = async () => {
+
+    const session = await auth.api.getSession({
+        headers: await headers() // you need to pass the headers object.
+    })
+
+    const user = session?.user
+    // console.log(user)
+
+    const res = await fetch(`http://localhost:5000/booking/${user?.id}`)
+    const data = await res.json()
+    console.log(data)
     return (
-        <div>
-            my-bookings
+        <div className='max-w-7xl mx-auto'>
+            <h2 className='text-3xl font-bold'> My Bookings</h2>
         </div>
     );
 };
